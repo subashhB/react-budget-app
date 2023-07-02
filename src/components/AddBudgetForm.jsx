b76129f314d514ab75b6ff80df1e3df78ee8cbd9
Admin <subashbaidya9@gmail.com>
2023-07-02 17:16:44 +0545

    feat: Clearing Form

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,11 +1,25 @@
 import { CurrencyRupeeIcon, PlusIcon } from "@heroicons/react/24/solid";
-import { Form } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { Form, useFetcher } from "react-router-dom";
 
 const AddBudgetForm = () => {
+  const fetcher = useFetcher();
+  const isSubmitting = fetcher.state === "submitting";
+
+  const formRef = useRef();
+  const focusRef = useRef();
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      formRef.current.reset();
+      focusRef.current.focus();
+    }
+  }, [isSubmitting]);
+
   return (
     <div className="form-wrapper">
       <h2 className="h3">Create Budget</h2>
-      <Form method="post" className="grid-sm">
+      <fetcher.Form method="post" className="grid-sm" ref={formRef}>
         <div className="grid-xs">
           <label htmlFor="newBudget">Budget Name</label>
           <input
@@ -14,6 +28,7 @@ const AddBudgetForm = () => {
             id="newBudget"
             placeholder="e.g. Groceries"
             required
+            ref={focusRef}
           />
         </div>
         <div className="grid-xs">
@@ -29,8 +44,17 @@ const AddBudgetForm = () => {
           />
         </div>
         <input type="hidden" name="_action" value="createBudget" />
-        <button type="submit" className="btn btn--dark"><PlusIcon width={20}/> <span>Create Budget</span></button>
-      </Form>
+        <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
+          {isSubmitting ? (
+            <span>Submitting...</span>
+          ) : (
+            <>
+              <PlusIcon width={20} />
+              <span>Create Budget</span>
+            </>
+          )}
+        </button>
+      </fetcher.Form>
     </div>
   );
 };
